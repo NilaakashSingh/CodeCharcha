55edaf4b2fec0eb1c8747258cbcd4693c4c8f758
Swapnil Salunke <swasalunke@deloitte.com>
2020-06-12 17:02:22 +0530

    Intial Commit for Blank project structure

---
+++ ComponentsProject/ComponentsProject/ComponentsProject/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  ComponentsProject
+//
+//  Created by Salunke, Swapnil Uday (US - Mumbai) on 12/06/20.
+//  Copyright © 2020 Swapnil Salunke. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+}
+
